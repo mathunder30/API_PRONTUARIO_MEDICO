f1cdfe6ce6c12c286fdbebc8282fda80895a5313
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-03-10 14:10:09 -0300

    buscar completa funcionando

--- backend/src/controllers/BuscarControllers.ts
+++ backend/src/controllers/BuscarControllers.ts
@@ -3,18 +3,28 @@ import Observacoes from "../models/Observacoes";
 import Medicacao from "../models/Medicacao";
 import Historico from "../models/Historico";
 export const BuscarTodosByID = async (req: Request, res: Response) => {
-    const paciente_id = (req.params.id, 10);
+    const paciente_id = parseInt(req.params.id, 10);
 
     try{
         const observacoes = await Observacoes.buscarObservacoesByID(Number(paciente_id));
         const medicamento = await Medicacao.buscarMedicacaoID(Number(paciente_id));
         const historico = await Historico.buscarHistoricoByID(Number(paciente_id));
 
-        if(observacoes || medicamento || historico){
-            res.status(200).json({observacoes});
-            res.status(200).json({medicamento});
-            res.status(200).json({historico});
+        if(observacoes && medicamento && historico){
+            
+            res.status(200).json({medicamento, historico, observacoes});
 
+        } else if(!observacoes){
+
+            res.status(400).json({message: ` Observações medicas para o paciente não encontradas!`});
+
+        } else if(!medicamento) {
+
+            res.status(400).json({message: `Medicamentos para o paciente não encontradas!`});
+
+        } else if (!historico){
+
+            res.status(400).json({message: `Historico medico do paciente não encontradas!`});
 
         } else{
             
