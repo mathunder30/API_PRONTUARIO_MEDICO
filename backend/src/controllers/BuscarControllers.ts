e4c7e042d100062e6791a6a40801ab765fd1892b
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-03-10 20:32:49 -0300

    ataulização de busca por todos. integrando procedimento

--- backend/src/controllers/BuscarControllers.ts
+++ backend/src/controllers/BuscarControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Observacoes from "../models/Observacoes";
 import Medicacao from "../models/Medicacao";
 import Historico from "../models/Historico";
+import Procedimentos from "../models/Procedimentos";
 export const BuscarTodosByID = async (req: Request, res: Response) => {
     const paciente_id = parseInt(req.params.id, 10);
 
@@ -9,10 +10,11 @@ export const BuscarTodosByID = async (req: Request, res: Response) => {
         const observacoes = await Observacoes.buscarObservacoesByID(Number(paciente_id));
         const medicamento = await Medicacao.buscarMedicacaoID(Number(paciente_id));
         const historico = await Historico.buscarHistoricoByID(Number(paciente_id));
+        const procedimento = await Procedimentos.buscarProcedimentoID(Number(paciente_id));
 
-        if(observacoes && medicamento && historico){
+        if(observacoes && medicamento && historico && procedimento){
             
-            res.status(200).json({medicamento, historico, observacoes});
+            res.status(200).json({medicamento, historico, observacoes, procedimento});
 
         } else if(!observacoes){
 
@@ -25,6 +27,10 @@ export const BuscarTodosByID = async (req: Request, res: Response) => {
         } else if (!historico){
 
             res.status(400).json({message: `Historico medico do paciente não encontradas!`});
+
+        }  else if (!procedimento){
+
+            res.status(400).json({message: `Procedimento medico do paciente não encontradas!`});
 
         } else{
             
