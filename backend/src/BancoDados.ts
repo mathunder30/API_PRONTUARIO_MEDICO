d5851c0a4bb461c0225f49b8f969972ec67d973e
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-03-11 10:21:15 -0300

    atualizações do dontenv

--- backend/src/BancoDados.ts
+++ backend/src/BancoDados.ts
@@ -1,11 +1,17 @@
 import mysql from 'mysql2';
+import dontenv from 'dotenv';
+
+dontenv.config();
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 'aluno',
-    database: 'prontuario_medico',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT), // Converter para número
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 
 });
 
