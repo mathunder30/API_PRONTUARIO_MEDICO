4d4f648fee2333bfd70434333cf70e7527f78d5b
mathunder30 <mateustlopes11@gmail.com>
2025-03-12 22:50:49 -0300

    atualizações

--- backend/src/models/Medico.ts
+++ backend/src/models/Medico.ts
@@ -1,5 +1,5 @@
 import { promisePool } from '../BancoDados';
-import msql2 from 'mysql2';
+import msql2 from "mysql2";
 
 interface  IMedico {
     id?: number,
@@ -11,3 +11,15 @@ interface  IMedico {
     senha: string
     criado_em: string
 }
+
+export default class Medico {
+
+    static  async CadastroMedico (medico: IMedico): Promise<IMedico> {
+        const [rows] = await promisePool.execute('INSERT INTO medico (nome, crm, especialidade, telefone, email, senha, criado_em) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [medico.nome, medico.crm, medico.especialidade, medico.telefone, medico.email, medico.senha, medico.criado_em]);
+        console.log(`Medico foi inserido ${rows}`);
+
+        return {...medico, id:(rows as msql2.ResultSetHeader).insertId}
+
+    };
+} 
