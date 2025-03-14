85ee967dffd3c3b81f4ba429d2a085261e040bbb
mathunder30 <mateustlopes11@gmail.com>
2025-03-14 00:09:31 -0300

    Atualização: classe medico

--- backend/src/models/Medico.ts
+++ backend/src/models/Medico.ts
@@ -14,6 +14,11 @@ interface IMedico {
 
 export default class Medico {
     static async  CadastroMedico (medico: IMedico): Promise<IMedico> {
-        const [rows] = await promisePool.execute('INSERT INTO medico (nome, crm, espe) VALUES()')
+        const [rows] = await promisePool.execute('INSERT INTO medico (nome, crm, especialidade, telefone, email, senha, criado_em) VALUES(?, ?, ?, ?, ?, ?, ?)',
+            [medico.nome, medico.crm, medico.especialidade, medico.telefone, medico.telefone, medico.email, medico.senha, medico.criado_em]);
+        console.log('Usuário inserido: ', rows);
+        return {...medico, id:(rows as msql2.ResultSetHeader).insertId}
+
+
     }
 }
